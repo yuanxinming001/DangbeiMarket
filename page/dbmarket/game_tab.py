693a645b0ed68f41c3f372f5ab1759f86840e907
yuanxinming <wb.yuanxinming@mesg.corp.netease.com>
2022-07-18 11:33:45 +0800

    丰富了游戏页面的元素内容

---
+++ page/dbmarket/game_tab.py
@@ -0,0 +1,35 @@
+# 游戏tab下面的所有元素记录
+
+# The remote control 遥控器游戏
+
+The_remote_control_game = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[1]'
+
+# The handle 手柄游戏
+
+The_handle_game = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[2]'
+
+# Empty rat 空鼠游戏
+
+Empty_rat_game = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[7]'
+
+# In the middle resources_one 中间资源的第一个
+
+inthe_middle_resources_one = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[3]'
+
+#  In the middle resources_one 中间资源的第二个
+
+inthe_middle_resources_two = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[4]'
+
+# In the down resources_one 中间资源的第一个
+
+inthe_down_resources_one = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[8]'
+
+inthe_down_resources_two = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[9]'
+
+# hot game 热门游戏
+
+hotgame = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[10]'
+
+# hot game down 热门游戏下面的童年经典游戏
+
+hot_game_down = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[12]'
