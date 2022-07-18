54784d3fda8935b5872d34185e893c15ffbdee08
yuanxinming <wb.yuanxinming@mesg.corp.netease.com>
2022-07-18 14:00:21 +0800

    一键诊断app新增用例

--- Testcase/testcase_akeydiagnostic.py
+++ Testcase/testcase_akeydiagnostic.py
@@ -2,7 +2,7 @@ import time
 import unittest
 from loguru import logger
 from appium import webdriver
-from page.dbmarket import home_page, my_tab
+from page.akeydiagnostic import logget,Network,Network_details
 from selenium.webdriver.common.by import By
 from APPinfo.Akey_diagnostic import akey_diagnostic_caps
 from PublicFunction.Public_board import remotecontrol
@@ -19,9 +19,44 @@ class Test_akeydiagonstic(unittest.TestCase):
         logger.info('--------------测试结束啦----------------')
 
 
-    # 检查当贝市场我的tab 热播影视点击可以进入资源详情页面；
+    # 检查打开一键诊断app，二维码和title显示正确
     def test_01(self):
         driver = self.driver
         time.sleep(5)
-        logger.info('这个app也可以喽')
-        remotecontrol(send='ok')
+        get_Network_inthe_diagnosis_of_button = driver.find_element(By, Network.Network_inthe_diagnosis_of_button)
+        time.sleep(3)
+        get_Network_inthe_diagnosis_of_title = driver.find_element(By, Network.Network_inthe_diagnosis_of_title)
+        if get_Network_inthe_diagnosis_of_button and get_Network_inthe_diagnosis_of_title:
+            logger.info('打开一键诊断app，二维码和title显示正确')
+        else:
+            logger.info('error：打开一键诊断app，二维码和title显示异常，请查看')
+
+    # 检查打开一键诊断app，网络诊断button 和 日志收集button 展示正确
+    def test_02(self):
+        driver = self.driver
+        time.sleep(5)
+        get01_Network_inthe_diagnosis_of_button = driver.find_element(By, Network.Network_inthe_diagnosis_of_button)
+        time.sleep(2)
+        get_logget_start_button = driver.find_element(By, logget.logget_start_button)
+        if get01_Network_inthe_diagnosis_of_button and get_logget_start_button:
+            logger.info('打开一键诊断app，网络诊断button 和 日志收集button 展示正确')
+        else:
+            logger.info('error：打开一键诊断app，没有发现网络诊断button 和 日志收集button ')
+
+    # 检查点击一键诊断button 跳转页面正确
+    def test_03(self):
+        driver = self.driver
+        time.sleep(5)
+        # click Network_inthe_diagnosis_of_button
+        driver.find_element(By, Network.Network_inthe_diagnosis_of_button).click()
+        time.sleep(60)
+        get_Network_Service_one = driver.find_element(By, Network_details.Network_Service_one)
+        get_back = driver.find_element(By, Network_details.back)
+        if get_back and get_Network_Service_one:
+            logger.info('点击一键诊断button 跳转页面正确')
+        else:
+            logger.info('error: 点击一键诊断button 跳转页面异常')
+
+
+
+
---
+++ page/akeydiagnostic/Network_details.py
@@ -0,0 +1,19 @@
+# 网络诊断详情页面
+
+# back 返回按钮
+
+back = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[5]/android.widget.TextView'
+
+# 网络正常服务
+
+Normal_Network_Service = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.TextView[1]'
+
+# 检查项title 【1】【2】【3】【4】
+
+Network_Service_one =  '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.TextView[1]'
+
+Network_Service_two = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.TextView[1]'
+
+Network_Service_three = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[3]/android.widget.TextView[1]'
+
+Network_Service_fore = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[4]/android.widget.TextView[1]'
