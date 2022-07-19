d6d8c75e8b007eda93cdca67018261b594c28252
yuanxinming <wb.yuanxinming@mesg.corp.netease.com>
2022-07-19 16:13:52 +0800

    add DB_market 影音、福利社元素填充

---
+++ page/dbmarket/av_tab.py
@@ -0,0 +1,50 @@
+# 影视tab av_tab
+
+# --------------------------------------------------------------------------------
+# (页面左侧一列数据元素)
+# --------------------------------------------------------------------------------
+# 视频点播 Video on demand
+Video_on_demand = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[1]'
+
+# 体育电竞 Sports e-sports
+Sports_esports = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[2]'
+
+# 音乐娱乐 Music_entertainment
+Music_entertainment = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[7]'
+
+
+
+# -------------------------------------------------------------------------------
+# 页面中间两列元素
+# --------------------------------------------------------------------------------
+
+# 中间两列，左面第一个元素 In the middle left one
+inthe_middle_left_1 = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[3]'
+
+# 中间两列，左面第二个元素 In the middle left two
+inthe_middle_left_2 = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[8]'
+
+# 中间两列，右面第2个元素 In the middle right one
+inthe_middle_right_1 = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[4]'
+
+# 中间两列，右面第2个元素 In the middle right two
+inthe_middle_right_2 = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[9]'
+
+
+
+# --------------------------------------------------------------------------------
+# (页面右侧一列数据元素)
+# --------------------------------------------------------------------------------
+
+# 热门影音模块 hotmv
+hotmv = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[10]'
+
+# 热播影视 hot play av
+hot_play_av = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[12]'
+
+# --------------------------------------------------------------------------------
+
+# 热门影视模块 hotmv resources 3  推荐资源元素 3
+hotmv_resources_3 = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.ImageView[11]'
+
+
